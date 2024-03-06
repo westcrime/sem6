871754a0af0d20e1adc6e52606f1526e34b90bdf
westcrime <westcrime77@rambler.ru>
2024-03-06 16:53:05 +0300

    fixed lab 4

--- osisp/lab4/self_healing_process.c
+++ osisp/lab4/self_healing_process.c
@@ -2,43 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#define NUMBER_OF_ITERATIONS 10
+#define NUMBER_OF_ITERATIONS 20
 
 void handler(int sig) {
     if (sig == SIGTERM) {
-        FILE *file;
-        file = fopen("buffer.txt", "r");
-        if (file == NULL) {
-            printf("Ошибка открытия файла.\n");
-            return;
-        }
-        char number[9];
-        // Считываем число из файла
-        if (fscanf(file, "%9s", number) != 1) { // Читаем не более 9 символов, чтобы избежать переполнения буфера
-            printf("Ошибка чтения числа из файла.\n");
-            fclose(file);
-            return;
-        }
-        number[9] = '\0'; // Устанавливаем конец строки
-
         pid_t child_pid = fork();
         if (child_pid == -1) return;
-        if (child_pid == 0) {
-            //signal(SIGTERM, handler);
-            // Код копии процесса
-            printf("Child process created.\n");
-            int i = atoi(number);
-
-            for (; i < NUMBER_OF_ITERATIONS; i++) {
-                // Write some text to the file
-                fprintf(file, "%d\n", i);
-                printf("Child process %d: %d\n", getpid(), i);
-                sleep(1);
-            }
-            exit(EXIT_SUCCESS);
-        } else {
-            fclose(file);
-            exit(EXIT_SUCCESS); // Копия завершает выполнение
+        if (child_pid > 0) {
+            exit(EXIT_SUCCESS); //Завершаем программу в основном процессе
         }
     }
 }
@@ -46,13 +17,7 @@ void handler(int sig) {
 int main() {
     signal(SIGTERM, handler);
     for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-
-        //Открытие буферного файла, запись в него числа, и вывод его в консоль
-        FILE *fptr;
-        fptr = fopen("buffer.txt", "w");
-        fprintf(fptr, "%d\n", i);
-        printf("Parent process %d: %d\n", getpid(), i);
-        fclose(fptr);
+        printf("Process %d: %d\n", getpid(), i);
         sleep(1);
     }
 
